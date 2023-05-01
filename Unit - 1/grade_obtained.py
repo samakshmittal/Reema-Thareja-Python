455d78805f8b0f846dc8468cbce8d9613fd35663
samakshmittal <samakshmittal826@gmail.com>
2023-05-01 12:15:03 +0530

    Create grade_obtained.py

---
+++ Unit - 1/grade_obtained.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first no"))
+if(a>75):
+    print("O")
+elif(a<=75 and a>=60):
+    print("A")
+elif(a<60 and a>=50):
+    print("B")
+elif(a<50 and a>=40):
+    print("C")
+else:
+    print("D")
